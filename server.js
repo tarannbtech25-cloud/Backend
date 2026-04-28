ce39b3e3ef6afb34d7152405830e776678c1ad66
tarannbtech25-cloud <tarannbtech25@rvu.edu.in>
2026-04-28 13:15:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT
-});SHOUL
+});
 
 // Test DB connection
 db.connect(err => {
